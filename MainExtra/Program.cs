76b01703b67727eaeb9e194dfb55f4e7e43f56eb
mainguyentuantu <tuantumn0103@gmail.com>
2024-09-21 12:18:47 +0700

    ex1

--- MainExtra/Program.cs
+++ MainExtra/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter days: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int weeks = number / 7;
+int days = number % 7;
+Console.WriteLine($"{number} days = {weeks} weeks and {days} days ");
